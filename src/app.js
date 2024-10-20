c3ce77080c4bdd06211e09dff0405532cd206702
Orden14 <t.lesrel@gmail.com>
2024-10-20 14:37:10 +0200

    change cors options

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const apiRouter = require("./routes");
 const cors = require("cors");
 
 const corsOptions = {
-    origin: "http://localhost:8001",
+    origin: true,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
